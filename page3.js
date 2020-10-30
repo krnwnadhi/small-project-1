a108709ca7425ff3bb2fe07672f7b4fdf6129a95
Adhi Kurniawan <adhikurniawan2108@gmail.com>
2020-10-31 00:59:46 +0700

    javascript page3.js

--- page3.js
+++ page3.js
@@ -3,8 +3,8 @@ const data = document.querySelectorAll(".kolom-utama div");
 
 
 search.addEventListener("keyup", function (event) {
-    // console.log(event);
     const q = event.target.value.toLowerCase();
+
     data.forEach((datas) => {
         datas.querySelector('p').textContent.toLowerCase().startsWith(q)
             ? (datas.style.display = '')
