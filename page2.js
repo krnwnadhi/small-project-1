bba5c45ac97eeb8015a67706783fc02a7f88a081
Adhi Kurniawan <adhikurniawan2108@gmail.com>
2020-10-30 20:17:23 +0700

    menghapus comment pada page2.js

--- page2.js
+++ page2.js
@@ -27,28 +27,3 @@ function decrNumber() {
     }
     document.getElementById("display").innerHTML = i;
 };
-
-// Akhir Fitur Checkout
-
-// Fitur Pop Up
-
-// function myFunction() {
-//     var popup = document.getElementById("myPopup");
-//     popup.classList.toggle("show");
-// }
-
-// Akhir Fitur Pop Up
-
-// IF
-
-// function fitur() {
-//     if (i = 0) {
-//         alert("Keranjangmu Kosong")
-//     } else if (i > 9) {
-//         alert("Keranjang Belanja Kamu penuh. Ayo Dong di Checkout")
-//     } else {
-//         alert("Ayo Dong di Checkout")
-//     }
-// }
-
-// Akhir IF
